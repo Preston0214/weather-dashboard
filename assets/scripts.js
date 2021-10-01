618b8742d4a8e9e5f8dd6b38b1c18c325f3e0a73
Preston <prestonconley5@gmail.com>
2021-09-30 22:08:37 -0500

    added functions to get weather data from api

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,30 +1,27 @@
-var cityName = "London";
-var lat;
-var lon;
+var cityName;
+var {lat} = location;
+var {lon} = location;
+var apiBaseUrl = 'https://api.openweathermap.org/';
+var searchBtn = $('#searchBtn');
 
 function getCoords(){
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=b078f64a50989fdeb7bf58c9372972ad'
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b078f64a50989fdeb7bf58c9372972ad`
     fetch(requestUrl)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data)
-        console.log(data.coord.lat)
         lat = data.coord.lat;
         lon = data.coord.lon;
         console.log(lon)
         console.log(lat)
+        getWeather();
     });
 }
 
-getCoords();
-
-
-// var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?q="+cityName+"&units=imperial&appid=b078f64a50989fdeb7bf58c9372972ad";
-
 function getWeather(){
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&units=imperial&appid=b078f64a50989fdeb7bf58c9372972ad'
+    var requestUrl = `${apiBaseUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=b078f64a50989fdeb7bf58c9372972ad`
     fetch(requestUrl)
     .then(function(response){
         return response.json();
@@ -35,4 +32,12 @@ function getWeather(){
     });
 }
 
-getWeather();
+function search(event){
+    cityName = $('#userInput').val()
+    event.preventDefault()
+    console.log('yes')
+    console.log(cityName)
+    getCoords()
+}
+
+searchBtn.on('click',search)
